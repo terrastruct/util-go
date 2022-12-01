8cc30ca56bbe1da498b946089b6e68c17772c4ff
Anmol Sethi <anmol@terrastruct.com>
2022-12-01 10:57:46 -0800

    xbrowser: Fix
    Updates #175
    Updates #205

--- xbrowser/xbrowser.go
+++ xbrowser/xbrowser.go
@@ -12,9 +12,12 @@ import (
 
 func Open(ctx context.Context, env *xos.Env, url string) error {
 	browserEnv := env.Getenv("BROWSER")
-	if browserEnv != "" {
-		browserSh := fmt.Sprintf("%s '$1'", browserEnv)
-		cmd := exec.CommandContext(ctx, "sh", "-c", browserSh, "--", url)
+	if browserEnv == "0" || browserEnv == "false" {
+		return nil
+	}
+	if browserEnv != "" && browserEnv != "1" && browserEnv != "true" {
+		browserSh := fmt.Sprintf(`%s "$1"`, browserEnv)
+		cmd := exec.CommandContext(ctx, "sh", "-sc", browserSh, "--", url)
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			return fmt.Errorf("failed to run %v (out: %q): %w", cmd.Args, out, err)
