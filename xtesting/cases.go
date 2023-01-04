f23342b1476fd0878c185a4acdbea01b5b5d9d4f
Anmol Sethi <anmol@terrastruct.com>
2023-01-03 17:10:24 -0800

    xtesting: Remove Skip
    Just use t.SkipNow directly

--- xtesting/cases.go
+++ xtesting/cases.go
@@ -9,7 +9,6 @@ import (
 
 type Case struct {
 	Name string
-	Skip bool
 
 	Run func(t *testing.T, ctx context.Context, env *xos.Env)
 }
@@ -18,10 +17,6 @@ func RunCases(t *testing.T, cases []Case) {
 	for _, tc := range cases {
 		tc := tc
 		t.Run(tc.Name, func(t *testing.T) {
-			if tc.Skip {
-				t.SkipNow()
-			}
-
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
